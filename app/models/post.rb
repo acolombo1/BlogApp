02dfeed709aeb1bf86af0ba72c77749470aeb5c2
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-08-11 20:23:30 -0300

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def last_5_comments
     comments.order(created_at: :desc).limit(5)
   end
