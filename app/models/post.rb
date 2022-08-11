bc82e235cb6c2d625fb5cde14d4a91559c185fc6
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-08-11 12:22:25 -0300

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', counter_cache: 'posts_counter'
   has_many :comments
   has_many :likes
-  def get_last_5_comments
+  def last_5_comments
     comments.order(created_at: :desc).limit(5)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
-  def get_last_3_posts
-    posts.limit(3).order(created_at: :desc)  
+  def last_3_posts
+    posts.limit(3).order(created_at: :desc)
   end
 end
