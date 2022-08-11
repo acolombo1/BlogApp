93e8fed8accc2ef5e77dab1b98a71287e23324dd
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-08-11 13:30:37 -0300

    add increment counter for posts, comments, likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', counter_cache: 'posts_counter'
+  belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
   def last_5_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  after_create do
+    inc_author_posts_counter
+  end
+
+  def inc_author_posts_counter
+    author.posts_counter.nil? ? author.update(posts_counter: 1) : author.update(posts_counter: author.posts_counter + 1)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   def last_3_posts
-    posts.limit(3).order(created_at: :desc)
+    posts.order(created_at: :desc).limit(3)
   end
 end
