f76cf1f6ff5f7c7c6325342a8257c87ec4c175b7
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-08-11 12:15:43 -0300

    Fix #inspect not allowed, added counter_cache

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', counter_cache: 'posts_counter'
   has_many :comments
   has_many :likes
   def get_last_5_comments
