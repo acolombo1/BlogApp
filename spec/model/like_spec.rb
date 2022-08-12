33f6ea8c2b6478cf93181d38170dd92ffb129c05
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-08-12 16:04:05 -0300

    Put likes and comments methods tests in like and comment specs

--- spec/model/like_spec.rb
+++ spec/model/like_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  # tests go here
+  before do
+    @user1 = User.create(name: 'Juan', photo: 'http://www.photos.com/user1/photo2.jpg', bio: 'My bio', posts_counter: 0)
+    @post1 = Post.create(author: @user1, title: 'Post 1', text: 'This is the first post', likes_counter: 0,
+                         comments_counter: 0)
+  end
+
+  it 'Adding a like to post should increment its likes_counter' do
+    counter = @post1.likes_counter
+    Like.create(author: @user1, post: @post1)
+    expect(@post1.likes_counter).to eq(counter + 1)
+  end
 end
