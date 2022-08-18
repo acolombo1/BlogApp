e0acddc8fcbb0e47a1e130205a12c0834153ea8d
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-08-18 15:29:50 -0300

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-first_user = User.create(name: 'Tom', photo: 'https://acolombo1.github.io/Portfolio/foto1.jpg', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'Lilly', bio: 'Teacher from Poland.')
+first_user = User.create(name: 'Tom', photo: 'https://acolombo1.github.io/Portfolio/foto1.jpg', bio: 'Teacher from Mexico.', posts_counter: 0)
+second_user = User.create(name: 'Lilly', bio: 'Teacher from Poland.', posts_counter: 0)
 
-first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-Post.create(author: first_user, title: 'Post nr 2', text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.')
-Post.create(author: first_user, title: 'Post nr 3', text: 'This is my 3rd post')
-Post.create(author: first_user, title: 'Post nr 4', text: 'This is my 4th post')
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0)
+Post.create(author: first_user, title: 'Post nr 2', text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.', likes_counter: 0, comments_counter: 0)
+Post.create(author: first_user, title: 'Post nr 3', text: 'This is my 3rd post', likes_counter: 0, comments_counter: 0)
+Post.create(author: first_user, title: 'Post nr 4', text: 'This is my 4th post', likes_counter: 0, comments_counter: 0)
 
 Comment.create(post: first_post, author: second_user, text: 'Hi Tom!')
 Comment.create(post: first_post, author: first_user, text: 'Hi, how are you?')
