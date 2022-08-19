19852459f1b0e846ca6ba99ce4be073f859fc9f2
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-08-19 18:43:11 -0300

    Add posts comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
   get "/users", to: "users#index", as: "users"
   get "/users/:id", to: "users#show", as: "user"
-  
+
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  post '/posts', to: 'posts#create', as: 'post'
+
+  get '/users/:user_id/posts/:id/new_comment', to: 'comments#new', as: 'new_comment'
+  post '/users/:user_id/posts/:id/comment', to: 'comments#create', as: 'comment'
+
+  post '/users/:user_id/posts/:id/like', to: 'likes#create', as: 'like'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+first_user = User.create(name: 'Tom', photo: 'https://acolombo1.github.io/Portfolio/foto1.jpg', bio: 'Teacher from Mexico.', posts_counter: 0)
+second_user = User.create(name: 'Lilly', bio: 'Teacher from Poland.', posts_counter: 0)
+
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0)
+Post.create(author: first_user, title: 'Post nr 2', text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.', likes_counter: 0, comments_counter: 0)
+Post.create(author: first_user, title: 'Post nr 3', text: 'This is my 3rd post', likes_counter: 0, comments_counter: 0)
+Post.create(author: first_user, title: 'Post nr 4', text: 'This is my 4th post', likes_counter: 0, comments_counter: 0)
+
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!')
+Comment.create(post: first_post, author: first_user, text: 'Hi, how are you?')
+Comment.create(post: first_post, author: second_user, text: 'Fine, thanks, and you?')
+Comment.create(post: first_post, author: first_user, text: 'I\'m ok, how are you going with your posts?')
+Comment.create(post: first_post, author: second_user, text: 'I still have none')
+Comment.create(post: first_post, author: first_user, text: 'I have four already')
