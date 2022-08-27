00cc6abb84106e694da4d665b78fce986d984f7d
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-08-27 09:51:05 -0300

    Fix typo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,5 +7,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       expose: ["Authorization"],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    )
   end
 end
